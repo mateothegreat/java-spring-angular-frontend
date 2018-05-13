7d26efc3425de8767c40170555c2c2cb031b7f92
Matthew Davis <matthew@appsoa.io>
2018-05-13 13:47:54 -0500

    GA

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
-//Install express server
+/*
+ * server.js is used for Heroku to start a web server.
+ * In the package.json file specify the start script:
+ *
+ *      "scripts": {
+ *          ..
+ *          "start": "node server.js",
+ *          ..
+ *      },
+ *
+ * You'll need to `npm install express` before launching
+ * on Heroku.
+ *
+ */
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+/*
+*
+* This will serve the static files from the dist directory.
+*
+* When Heroku builds your repository it will run the
+* postinstall package.json script which will build the dist
+* distribution.
+*
+*/
 app.use(express.static(__dirname + '/dist'));
-
 app.get('/*', function (req, res) {
 
     res.sendFile(path.join(__dirname + '/dist/index.html'));
+
 });
 
-// Start the app by listening on the default Heroku port
+/*
+ * Heroku will listen map requests to port 8080 by default.
+ *
+ * Alternatively, the port can be passed by an environment
+ * variable.
+ *
+ */
 app.listen(process.env.PORT || 8080);
